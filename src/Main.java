3d3027756da8473d85118ed4d0ad16e807c6d8ca
Aava Sapkota <aavachhoir@gmail.com>
2020-05-25 10:11:00 -0400

    main class setup

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+/**
+ * Creators: Aava Sapkota and Sarah Zhao
+ * Class: ICS4UO-1
+ * Teacher: Ms.Krasteva
+ * Project: ISP
+ */
+
+import java.awt.*; 
+import java.awt.event.*; 
+import javax.swing.*;
+import javax.swing.JFrame.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.JComponent;
+import java.awt.Graphics;
+public class Main{
+
+    public void introduction(){
+        
+    }
+
+    public void mainMenu(){
+
+    }
+
+    public void highschool(){
+
+    }
+
+    public void learn(){
+
+    }
+
+    public void play(){
+
+    }
+
+    public void goodbye(){
+
+    }
+
+    public void credits(){
+        
+    }
+
+
+
+    /**
+     * Main method
+     * @param args
+     */
+    public static void main(String args[]){
+
+    }
+}
