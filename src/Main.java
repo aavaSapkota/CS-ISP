ddc4d64fcc68d653da4bebfd2d0263181ba55dc0
Aava Sapkota <aavachhoir@gmail.com>
2020-05-26 14:34:39 -0400

    Main Menu setup

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,12 @@ import javax.swing.JFrame.*;
 import java.io.*;
 import java.util.*;
 import javax.swing.JComponent;
+import javax.imageio.ImageIO;
 import java.awt.Graphics;
 
-public class Main  {
+public class Main {
     static Screen screen;
-    static UserInput input; 
+    static UserInput input;
     static JFrame mainScreen = new JFrame("Game Title");
 
     public Main() {
@@ -29,39 +30,49 @@ public class Main  {
         mainScreen.setSize(700, 500);
         mainScreen.setVisible(true);
         mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        input = new UserInput(mainScreen,screen);
-        
+        input = new UserInput(mainScreen, screen);
+
     }
 
-
     public void introduction() {
-        JLabel title = new JLabel("<html><div style='text-align: center; margin-top: 50px'> PHOENIX GAMES </div></html>");
+        mainScreen.getContentPane().removeAll();
+        //source: https://stackoverflow.com/questions/6810581/how-to-center-the-text-in-a-jlabel
+        JLabel title = new JLabel(
+                "<html><div style='text-align: center; margin-top: 50px'> PHOENIX GAMES </div></html>");
         mainScreen.add(title);
-        mainScreen.getContentPane().setBackground(Color.gray);
-        try{
+        // mainScreen.getContentPane().setBackground(Color.gray);
+        
+        try {
+
+            Image backgroundImage = ImageIO.read(new File("Menu Page.jpg"));
+            java.net.URL imgUrl = Main.class.getResource("Menu Page.jpg");
+            ImageIcon icon = new ImageIcon(imgUrl);
+            JLabel l = new JLabel(icon);
+            JLabel i = new JLabel("MAIN MENU");
+            mainScreen.add(l);
+            mainScreen.add(i);
             Thread.sleep(1000);
-        }catch(Exception a){};
+        } catch (Exception a) {
+        }
+        ;
         mainScreen.remove(title);
 
     }
 
     public void mainMenu() {
-        JPanel menu = new JPanel(new FlowLayout());
-        JLabel title = new JLabel(" Main Menu:", SwingConstants.CENTER);
-        JLabel learn = new JLabel(" Learn ");
-        JLabel play = new JLabel("Play ");
-        JLabel exit = new JLabel("Exit ");
-        menu.add(title);
-        menu.add(play);
-        menu.add(learn);
-        menu.add(exit);
-        mainScreen.add(menu);
-        mainScreen.getContentPane().setBackground(Color.cyan);
-        try{
+        try {
+            //source: https://stackoverflow.com/questions/13011705/how-to-add-an-imageicon-to-a-jframe
+            Image backgroundImage = ImageIO.read(new File("Menu Page.jpg"));
+            java.net.URL imgUrl = Main.class.getResource("Menu Page.jpg");
+            ImageIcon icon = new ImageIcon(imgUrl);
+            JLabel l = new JLabel(icon);
+            JLabel i = new JLabel("MAIN MENU");
+            mainScreen.add(l);
+            mainScreen.add(i);
             Thread.sleep(1000);
-        }catch(Exception a){};
-        mainScreen.remove(menu);
+        } catch (Exception a) {}
         
+
     }
 
     public void highscores() {
@@ -91,27 +102,28 @@ public class Main  {
      */
     public static void main(String args[]) {
         Main m = new Main();
-        
+
         while (screen.getScreen() <= 7) {
-            if (screen.getScreen()==1) {
+            if (screen.getScreen() == 1) {
                 m.introduction();
-            } else if (screen.getScreen()==2) {
+            } else if (screen.getScreen() == 2) {
                 m.mainMenu();
-            } else if (screen.getScreen()==3) {
+            } else if (screen.getScreen() == 3) {
                 m.highscores();
-            } else if (screen.getScreen()==4) {
+            } else if (screen.getScreen() == 4) {
                 m.learn();
-            } else if (screen.getScreen()==5) {
+            } else if (screen.getScreen() == 5) {
                 m.play();
-            } else if (screen.getScreen()== 6) {
+            } else if (screen.getScreen() == 6) {
                 m.goodbye();
-            } else if (screen.getScreen()==7) {
+            } else if (screen.getScreen() == 7) {
                 m.credits();
             }
             System.out.println(screen.getScreen());
 
         }
-        // source: https://stackoverflow.com/questions/1234912/how-to-programmatically-close-a-jframe
+        // source:
+        // https://stackoverflow.com/questions/1234912/how-to-programmatically-close-a-jframe
         mainScreen.dispatchEvent(new WindowEvent(mainScreen, WindowEvent.WINDOW_CLOSING));
     }
 }
