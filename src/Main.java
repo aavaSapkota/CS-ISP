a5791f8672bce4a2588050f4493d074ab2618143
Aava Sapkota <aavachhoir@gmail.com>
2020-05-26 08:38:21 -0400

    main menu starter

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 /**
  * Creators: Aava Sapkota and Sarah Zhao
  * Class: ICS4UO-1
@@ -8,36 +9,37 @@
  * THIS IS TO SEE IF YOU CAN SEE THIS
  */
 
-import java.awt.*; 
-import java.awt.event.*; 
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.JFrame.*;
 import java.io.*;
 import java.util.*;
 import javax.swing.JComponent;
 import java.awt.Graphics;
-public class Main implements ActionListener{
-    static int screen; 
-    JFrame mainScreen = new JFrame("Game Title");
-    JButton learn,play,highscores,next; 
-    
 
-    public Main(){
+public class Main implements ActionListener {
+    static int screen;
+    static JFrame mainScreen = new JFrame("Game Title");
+    JButton learn, play, highscores, next;
+
+    public Main() {
         next = new JButton("NEXT");
-        screen = 0; 
-        mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen = 0;
         next.addActionListener(this);
         mainScreen.add(next);
-        mainScreen.setLayout(new FlowLayout());
-        mainScreen.setSize(1000,1000); 
-        mainScreen.setVisible(true);
     }
 
-    public void actionPerformed(final ActionEvent e){
-        if(e.getSource()==next){
+    public void pauseProgram(){
+        JFrame pauseScreen = new JFrame("Pause");
+        mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(final ActionEvent e) {
+        if (e.getSource() == next) {
             screen++;
-        }else if(screen==2&&e.getSource()==learn){
-            screen = 4; 
+        } else if (screen == 2 && e.getSource() == learn) {
+            screen = 4;
             mainScreen.remove(learn);
             mainScreen.remove(play);
             mainScreen.remove(highscores);
@@ -45,53 +47,69 @@ public class Main implements ActionListener{
 
     }
 
-    public void introduction(){
+    public void introduction() {
         mainScreen.getContentPane().setBackground(Color.gray);
+
+    }
+
+    public void mainMenu() {
+        mainScreen.getContentPane().setBackground(Color.blue);
+        learn = new JButton("Learn");
+        play = new JButton("play");
+        highscores = new JButton("Highscores");
         mainScreen.add(learn);
         mainScreen.add(play);
         mainScreen.add(highscores);
     }
 
-    public void mainMenu(){
-        mainScreen.getContentPane().setBackground(Color.blue);
-    }
-
-    public void highscores(){
+    public void highscores() {
         mainScreen.getContentPane().setBackground(Color.red);
     }
 
-    public void learn(){
+    public void learn() {
         mainScreen.getContentPane().setBackground(Color.orange);
     }
 
-    public void play(){
-        mainScreen.getContentPane().setBackground(Color.yellow); 
+    public void play() {
+        mainScreen.getContentPane().setBackground(Color.yellow);
     }
 
-    public void goodbye(){
+    public void goodbye() {
         mainScreen.getContentPane().setBackground(Color.lightGray);
     }
 
-    public void credits(){
+    public void credits() {
         mainScreen.getContentPane().setBackground(Color.black);
     }
 
     /**
      * Main method
+     * 
      * @param args
      */
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Main m = new Main();
-        while(screen!=6){
-            if(screen==1)m.introduction();
-            if(screen==2) m.mainMenu();
-            if(screen==3) m.highscores();
-            if(screen==4) m.learn();
-            if(screen==5) m.play();
-            if(screen==6) m.goodbye();
-            if(screen==7) m.credits();
+        mainScreen.setLayout(new FlowLayout());
+        mainScreen.setSize(700, 700);
+        mainScreen.setVisible(true);
+        while (screen != 6) {
+            if (screen == 1) {
+                m.introduction();
+            } else if (screen == 2) {
+                m.mainMenu();
+            } else if (screen == 3) {
+                m.highscores();
+            } else if (screen == 4) {
+                m.learn();
+            } else if (screen == 5) {
+                m.play();
+            } else if (screen == 6) {
+                m.goodbye();
+            } else if (screen == 7) {
+                m.credits();
+            }
             System.out.println(screen);
-            
+
         }
     }
 }
